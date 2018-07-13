80817c4e664774d27336eeaec5cf826dbe56d3c0
nicekingwei <nicekingwei@foxmail.com>
2018-07-13 21:07:50 +0800

    base

--- task.py
+++ task.py
@@ -3,13 +3,13 @@ import time
 
 url = 'https://api.github.com/search/repositories?q=stars%3A'
 base = 100
-step = base/10
+step = int(base/10)
 k = 0
 for i in range(base,base*10,step):
     r = requests.get(url+str(i)+".."+str(i+step))
     repostories = r.json()
     num = repostories["total_count"]
-    print(str(i)+":"+str(num)+"\n")
+    print(str(i)+":"+str(num))
     k+=1
     if(k==9):
         k=0
